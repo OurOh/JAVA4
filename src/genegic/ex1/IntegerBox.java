884edf51d45e011dac3090afd2cb14b3ea7b23fb
OurOh <ahn980321@gmail.com>
2025-01-21 23:44:50 +0900

    0121

---
+++ src/genegic/ex1/IntegerBox.java
@@ -0,0 +1,15 @@
+package genegic.ex1;
+
+public class IntegerBox {
+    // 숫자를 보관
+    private Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get(){
+        return value;
+    }
+
+}
---
+++ src/genegic/ex1/StringBox.java
@@ -0,0 +1,14 @@
+package genegic.ex1;
+
+public class StringBox {
+    //문자열을 보관
+    private String value;
+
+    public void set(String object) {
+        this.value = object;
+    }
+    public String get() {
+        return value;
+    }
+
+}
