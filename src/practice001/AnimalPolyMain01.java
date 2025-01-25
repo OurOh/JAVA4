c24ecaf402ae4272f01f8bf73d03986938e4b62b
OurOh <ahn980321@gmail.com>
2025-01-25 09:54:59 +0900

    0125

---
+++ src/practice001/AnimalPolyMain01.java
@@ -0,0 +1,19 @@
+package practice001;
+
+public class AnimalPolyMain01 {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        dog.soundAnimal();
+        cat.soundAnimal();
+        caw.soundAnimal();
+
+
+    }
+
+
+}
---
+++ src/practice001/Cat.java
@@ -0,0 +1,10 @@
+package practice001;
+
+public class Cat extends Animal{
+
+    @Override
+    public void sound(){
+        System.out.println("멍멍");
+    }
+
+}
